f1b6ce96817e9a181dc9886bdecf9d3cd3f1813a
decaypark <decay@rnbsoft.com>
2022-08-23 18:05:52 +0900

    3

--- msa-user-api/src/main/java/msa/api/user/MsaUserApiApplication.java
+++ msa-user-api/src/main/java/msa/api/user/MsaUserApiApplication.java
@@ -1,6 +1,8 @@
 package msa.api.user;
 
+import msa.api.user.controller.CodeRepository;
 import msa.api.user.controller.MemberRepository;
+import msa.api.user.vo.Code;
 import msa.api.user.vo.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,6 +17,9 @@ public class MsaUserApiApplication implements CommandLineRunner {
 	@Autowired
 	private MemberRepository memberRepository;
 
+	@Autowired
+	private CodeRepository codeRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MsaUserApiApplication.class, args);
 	}
@@ -23,6 +28,8 @@ public class MsaUserApiApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		memberRepository.save(Member.join("member1"));
 		memberRepository.save(Member.join("member2"));
+
+		codeRepository.save(Code.join("code1", "codename1"));
 	}
 
 }
--- msa-user-api/src/main/java/msa/api/user/controller/UserController.java
+++ msa-user-api/src/main/java/msa/api/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package msa.api.user.controller;
 
+import msa.api.user.vo.Code;
+import msa.api.user.vo.CodeUser;
 import msa.api.user.vo.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,6 +18,12 @@ public class UserController {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    CodeRepository codeRepository;
+
+//    @Autowired
+//    CodeRepository codeRepository;
 
     @ResponseBody
     @GetMapping("/api/users1")
@@ -34,4 +42,70 @@ public class UserController {
     public List<Member> getUsers(HttpServletRequest request) {
         return memberRepository.findAll();
     }
+
+    @ResponseBody
+    @GetMapping("/api/codes")
+    public List<Code> getCodes(HttpServletRequest request) {
+        return codeRepository.findAll();
+    }
+
+    @ResponseBody
+    @GetMapping("/api/codeUsers")
+    public List<CodeUser> getCodeUsers(HttpServletRequest request) {
+
+        List<CodeUser> codeUsers = new ArrayList<CodeUser>();
+
+        CodeUser codeUser = new CodeUser();
+        codeUsers.add(codeUser);
+
+        codeUser.setCode(codeRepository.findAll().get(0));
+        codeUser.setMember(memberRepository.findAll().get(0));
+
+        return codeUsers;
+    }
+
+    @ResponseBody
+    @GetMapping("/api/codeUserEx1")
+    public CodeUser getCodeUserEx1(HttpServletRequest request) {
+
+        CodeUser codeUser = new CodeUser();
+
+        codeUser.setCode(codeRepository.findAll().get(0));
+        codeUser.setMember(memberRepository.getOne(Long.valueOf(2)));
+
+        return codeUser;
+    }
+
+    @ResponseBody
+    @GetMapping("/api/userEx1")
+    public List<Member> getUserEx1(HttpServletRequest request) {
+
+        return memberRepository.selectAllJPQL1();
+    }
+
+    @ResponseBody
+    @GetMapping("/api/userEx2")
+    public List<Member> getUserEx2(HttpServletRequest request) {
+
+        return memberRepository.selectAllSQL1();
+    }
+
+    @ResponseBody
+    @GetMapping("/api/userEx3")
+    public List<Member> getUserEx3(HttpServletRequest request) {
+
+        return memberRepository.selectSQLById2(Long.valueOf(3));
+    }
+
+    @ResponseBody
+    @GetMapping("/api/userEx4")
+    public List<Member> getUserEx4(HttpServletRequest request) {
+
+        Member member = new Member();
+
+        member.setId(Long.valueOf(2));
+
+        return memberRepository.selectSQLById3(member);
+    }
+
 }
