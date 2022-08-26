0b7de577f562433620c0ea074aabee480a431a5a
decaypark <decay@rnbsoft.com>
2022-08-26 17:23:17 +0900

    f

--- msa-test3-client/src/main/frontend/src/popup/BasicModal.js
+++ msa-test3-client/src/main/frontend/src/popup/BasicModal.js
@@ -16,7 +16,7 @@ const style = {
   p: 4,
 };
 
-export default function BasicModal() {
+export default function BasicModal({ title, content }) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -32,10 +32,10 @@ export default function BasicModal() {
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+            {title}
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            {content}
           </Typography>
         </Box>
       </Modal>
